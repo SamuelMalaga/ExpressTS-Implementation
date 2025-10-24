f94bec31179d31f936e8bda68b0155e64592f4b2
Samuel Mendes Malaga <samuelmalaga99@gmail.com>
2025-10-24 21:44:17 +0200

    Test: started the implementation of unit testing of the controller

--- tests/itemController.test.ts
+++ tests/itemController.test.ts
@@ -1,1 +1,25 @@
 import { Request, Response } from "express";
+import ItemController from '../src/controllers/itemController';
+import itemRepository from "../src/repositories/itemRepository";
+
+
+describe('Item controller tests', () => {
+    let mockItemRepository: jest.Mocked<itemRepository>;
+    let controller: ItemController
+
+    let mockRequest = (body = {}, params ={}, query ={}) => ({
+        body,
+        params,
+        query
+    })
+
+    const mockResponse = () => {
+        const res: any = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+    }
+
+    const mockNext = jest.fn()
+
+})
